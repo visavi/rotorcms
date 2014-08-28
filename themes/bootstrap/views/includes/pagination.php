04fe6af6617965d3996c6fd6d7ae4838112deb16
visavi <visavi.net@mail.ru>
2014-08-28 11:39:02 +0400

    fix pagination

--- themes/bootstrap/views/includes/pagination.php
+++ themes/bootstrap/views/includes/pagination.php
@@ -1,7 +1,7 @@
 <ul class="pagination">
 	<?php foreach($pages as $page): ?>
 		<?php if(isset($page['separator'])): ?>
-			<li><?= $page['name'] ?></li>
+			<li><span><?= $page['name'] ?></span></li>
 		<?php elseif(isset($page['current'])): ?>
 			<li class="active"><span><?= $page['name'] ?></span></li>
 		<?php else: ?>
