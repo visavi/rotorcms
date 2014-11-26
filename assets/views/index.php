1ecf01e7f31ee8342d1c78a540b45c86e45134ce
visavi <visavi.net@mail.ru>
2014-11-26 13:54:34 +0300

    Поправил главную

--- assets/views/index.php
+++ assets/views/index.php
@@ -1,26 +1,36 @@
 <?php $config['newtitle'] = 'Главная страница'; ?>
 
 <ul class="nav nav-pills nav-stacked">
-	<li class="index-link"><a href="/news"><span class="fa fa-circle-o"></span> Новости сайта</a> (<?=stats_news()?>)</li> <?=last_news()?></li>
+	<li class="index-link"><a href="/news"><span class="fa fa-circle-o"></span> Новости сайта (<?=stats_news()?>)</a></li> <?=last_news()?></li>
+</ul>
 
+<ul class="nav nav-pills nav-stacked">
 	<li class="index-link"><a href="/pages/index.php?act=recent"><span class="fa fa-circle-o"></span> Общение</a></li>
+	<li class="index-link"><a href="/book"><span class="fa fa-circle-o"></span> Гостевая книга (<?=stats_guest()?>)</a></li>
+	<li class="index-link"><a href="/gallery"><span class="fa fa-circle-o"></span> Фотогалерея (<?=stats_gallery()?>)</a></li>
 
-	<li class="index-link"><a href="/book"><span class="fa fa-circle-o"></span> Гостевая книга</a> (<?=stats_guest()?>)</li>
-	<li class="index-link"><a href="/gallery"><span class="fa fa-circle-o"></span> Фотогалерея</a> (<?=stats_gallery()?>)</li>
+	<li class="index-link"><a href="/forum"><span class="fa fa-circle-o"></span> Форум (<?=stats_forum()?>)</a></li>
+</ul>
 
-	<li class="index-link"><a href="/forum"><span class="fa fa-circle-o"></span> Форум</a> (<?=stats_forum()?>)</li>
+<ul class="nav nav-pills nav-stacked">
 	<?=recenttopics()?>
+</ul>
 
-	<li class="index-link"><a href="/load"><span class="fa fa-circle-o"></span> Загрузки</a> (<?=stats_load()?>)</li>
+<ul class="nav nav-pills nav-stacked">
+	<li class="index-link"><a href="/load"><span class="fa fa-circle-o"></span> Загрузки (<?=stats_load()?>)</a></li>
 	<?=recentfiles()?>
+</ul>
 
-	<li class="index-link"><a href="/blog"><span class="fa fa-circle-o"></span> Блоги</a> (<?=stats_blog()?>)</li>
+<ul class="nav nav-pills nav-stacked">
+	<li class="index-link"><a href="/blog"><span class="fa fa-circle-o"></span> Блоги (<?=stats_blog()?>)</a></li>
 	<?=recentblogs()?>
+</ul>
 
+<ul class="nav nav-pills nav-stacked">
 	<li class="index-link"><a href="/pages/index.php"><span class="fa fa-circle-o"></span> Сервисы сайта</a></li>
 	<li class="index-link"><a href="/mail"><span class="fa fa-circle-o"></span> Обратная связь</a></li>
-	<li class="index-link"><a href="/pages/userlist.php"><span class="fa fa-circle-o"></span> Список юзеров</a> (<?=stats_users()?>)</li>
-	<li class="index-link"><a href="/pages/adminlist.php"><span class="fa fa-circle-o"></span> Администрация</a> (<?=stats_admins()?>)</li>
+	<li class="index-link"><a href="/pages/userlist.php"><span class="fa fa-circle-o"></span> Список юзеров (<?=stats_users()?>)</a></li>
+	<li class="index-link"><a href="/pages/adminlist.php"><span class="fa fa-circle-o"></span> Администрация (<?=stats_admins()?>)</a></li>
 	<li class="index-link"><a href="/pages/index.php?act=stat"><span class="fa fa-circle-o"></span> Информация</a></li>
 	<li class="index-link"><a href="/pages/index.php?act=partners"><span class="fa fa-circle-o"></span> Партнеры и друзья</a></li>
 </ul>
