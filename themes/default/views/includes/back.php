e6da1c87c32d4ce9b9ecc3e7ae4d4e76fe412687
visavi <visavi.net@mail.ru>
2014-12-28 19:08:28 +0300

    Поправил стили возврата назад

--- themes/default/views/includes/back.php
+++ themes/default/views/includes/back.php
@@ -1,2 +1,2 @@
 <?php $icon = (isset($icon)) ? $icon : 'fa-arrow-circle-left'; ?>
- <a class="link" href="<?=$link?>"><span class="fa <?= $icon ?>"></span> <?=$title?></a>
+ <a class="short-link" href="<?=$link?>"><span class="fa <?= $icon ?>"></span> <?=$title?></a>
