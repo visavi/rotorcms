8662b39c18e84096abb71c8fab60ec4ae420e931
visavi <visavi.net@mail.ru>
2015-03-23 16:49:19 +0300

    Удалил функции которые уже есть

--- app/classes/App.php
+++ app/classes/App.php
@@ -141,49 +141,4 @@ class App
 		}
 		return $ret;
 	}
-
-	/**
-	 * Limit the number of characters in a string.
-	 *
-	 * @param  string  $value
-	 * @param  int     $limit
-	 * @param  string  $end
-	 * @return string
-	 */
-	public static function limit($value, $limit = 100, $end = '...')
-	{
-		if (mb_strlen($value) <= $limit) return $value;
-
-		return rtrim(mb_substr($value, 0, $limit, 'UTF-8')).$end;
-	}
-
-	/**
-	 * Convert the given string to lower-case.
-	 *
-	 * @param  string  $value
-	 * @return string
-	 */
-	public static function lower($value)
-	{
-		return mb_strtolower($value);
-	}
-
-	/**
-	 * Limit the number of words in a string.
-	 *
-	 * @param  string  $value
-	 * @param  int     $words
-	 * @param  string  $end
-	 * @return string
-	 */
-	public static function words($value, $words = 100, $end = '...')
-	{
-		preg_match('/^\s*+(?:\S++\s*+){1,'.$words.'}/u', $value, $matches);
-
-		if ( ! isset($matches[0])) return $value;
-
-		if (strlen($value) == strlen($matches[0])) return $value;
-
-		return rtrim($matches[0]).$end;
-	}
 }
