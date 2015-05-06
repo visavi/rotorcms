df72f46cab313811b8df29cabdcd06046ea02588
visavi <visavi.net@mail.ru>
2015-05-06 17:17:21 +0300

    fix travis2

--- app/tests/ForumTest.php
+++ app/tests/ForumTest.php
@@ -2,6 +2,23 @@
 
 class ForumTest extends PHPUnit_Framework_TestCase
 {
+	private $db;
+
+	public function __construct($db)
+	{
+		$this->db = $db;
+		if (!isset(static::$instances))
+		{
+			$tables = file_get_contents(APP.'/database/migration/tables/tables.sql');
+
+			foreach (explode(';', $tables as $sql)
+			{
+				if (trim($sql) != '')
+					$this->db->query($sql);
+			}
+		}
+	}
+
 	public function testGetAll()
 	{
 		$forums = Forum::getAll();
