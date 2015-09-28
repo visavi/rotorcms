cea3f8d76060a823123e34fab7a6ce28a7593449
visavi <visavi.net@mail.ru>
2015-09-28 18:32:09 +0300

    update jquery

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -119,7 +119,7 @@
 	</div>
 
 	@section('scripts')
-		<script src="/assets/js/jquery-1.11.2.min.js"></script>
+		<script src="/assets/js/jquery-1.11.3.min.js"></script>
 		<script src="/assets/js/bootstrap.min.js"></script>
 		<script src="/assets/js/notify.min.js"></script>
 		<script src="/assets/js/prettify.js"></script>
