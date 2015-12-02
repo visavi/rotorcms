4a546212e94deb5dabaa0d31e3786f00d85f6fc4
visavi <visavi.net@mail.ru>
2015-12-03 01:11:14 +0300

    сид настроек

---
+++ app/database/seeds/SettingsSeeder.php
@@ -0,0 +1,70 @@
+<?php
+
+use Phinx\Seed\AbstractSeed;
+
+class SettingsSeeder extends AbstractSeed
+{
+	/**
+	 * Run Method.
+	 */
+	public function run()
+	{
+		$data = [
+			[
+				'name'  => 'email',
+				'value' => 'visavi.net@mail.ru',
+			],
+			[
+				'name'  => 'admin',
+				'value' => 'Админ',
+			],
+			[
+				'name'  => 'sitelink',
+				'value' => 'rotorcms.ll',
+			],
+			[
+				'name'  => 'sitename',
+				'value' => 'Сайт на движке Rotor',
+			],
+			[
+				'name'  => 'sitetitle',
+				'value' => 'Rotor 5.0',
+			],
+			[
+				'name'  => 'version',
+				'value' => '5.0',
+			],
+			[
+				'name'  => 'guestbook_per_page',
+				'value' => 10,
+			],
+			[
+				'name'  => 'users_per_page',
+				'value' => 10,
+			],
+			[
+				'name'  => 'topics_per_page',
+				'value' => 10,
+			],
+			[
+				'name'  => 'posts_per_page',
+				'value' => 10,
+			],
+			[
+				'name'  => 'news_per_page',
+				'value' => 10,
+			],
+			[
+				'name'  => 'description',
+				'value' => 'Описание',
+			],
+			[
+				'name'  => 'keywords',
+				'value' => 'Ключевые слова',
+			],
+		];
+
+		$settings = $this->table('settings');
+		$settings->insert($data)->save();
+	}
+}
